328a8b8f4c96c041a3ccafd7862de8ca61941f55
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-16 17:55:39 +0600

    clean code/add TheHeader/change favicon

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,31 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <the-header />
+    <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+  import TheHeader from './components/TheHeader.vue';
+  export default {
+    name: 'App',
+    components: {TheHeader}
   }
-}
+</script>
+
+<style lang="scss">
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <nav class="header__nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  .header {
+    padding: 30px;
+    &__nav {
+      a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+          color: #42b983;
+        }
+      }
+    }
+  }
+</style>
