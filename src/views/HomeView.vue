654f9a46d35e10066926298f31b3162188f554d9
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-17 22:29:59 +0600

    add layout, styles and logic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,82 @@
 <template>
   <div class="home">
-   
+    <h1>Search cities</h1>
+    <search-cities @submit="searchSubmit" />
+    <cities-table
+      class="home__cities"
+      v-if="citiesList && !citiesError && !loading"
+      :cities="citiesList"
+      @cityClick="cityClickHandler"
+    />
+    <p class="home__error" v-else-if="!citiesList && citiesError && !loading">
+      {{ citiesError }}
+    </p>
+    <the-loader v-else-if="loading" />
+    <transition name="fade">
+      <info-modal v-if="isModal" @close="isModal = false" :city="currentCity" />
+    </transition>
   </div>
 </template>
 
-<script lang="ts">
+<script>
+  import CitiesTable from '@/components/CitiesTable.vue';
+  import SearchCities from '@/components/SearchCities.vue';
+  import InfoModal from '@/components/InfoModal.vue';
+  import TheLoader from '@/components/TheLoader.vue';
   export default {
     name: 'HomeView',
     components: {
-     
+      CitiesTable,
+      SearchCities,
+      InfoModal,
+      TheLoader,
+    },
+    data() {
+      return {
+        loading: false,
+        isModal: false,
+        currentCity: null,
+      };
+    },
+    methods: {
+      async searchSubmit(e) {
+        this.loading = true;
+        await this.$store.dispatch('getCities', { city: e.searchValue });
+        this.loading = false;
+      },
+      cityClickHandler(e) {
+        this.isModal = true;
+        this.currentCity = e.city;
+      },
+    },
+    computed: {
+      citiesList() {
+        return this.$store.getters.citiesList;
+      },
+      citiesError() {
+        return this.$store.getters.error;
+      },
+    },
+    watch: {
+      isModal(val) {
+        if (val) {
+          document.body.style.overflow = 'hidden';
+        } else {
+          document.body.style.overflow = 'visible';
+        }
+      },
     },
   };
 </script>
+
+<style scoped lang="scss">
+  .home {
+    &__cities {
+      margin: 20px auto;
+    }
+    &__error {
+      margin-top: 50px;
+      color: var(--warning-color);
+    }
+  }
+</style>
