bf5f4469471c82162aac165da745f65092385ee1
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-17 22:28:03 +0600

    add BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button class="button" :class="buttonColor" :type="type">{{ title }}</button>
+</template>
+
+<script>
+  export default {
+    name: 'Base-Button',
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      type: {
+        type: String,
+        required: false,
+        default: 'submit'
+      }
+    },
+    computed: {
+      buttonColor () {
+        return {
+          'button_submit': this.type === 'submit',
+          'button_reset': this.type === 'reset'
+        }
+      }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+  .button {
+    font-weight: 600;
+    padding: 5px 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    color: var(--button-text-color);
+    &_submit{
+      background-color: var(--additive-color);
+    }
+    &_reset{
+      background-color: var(--warning-color);
+    }
+    &:hover {
+      filter: brightness(1.1)
+    }
+  }
+</style>
