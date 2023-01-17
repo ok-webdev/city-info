6496eeee67fd1e1bb111b07a3b6601687e244e3e
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-17 19:08:17 +0600

    add store (state, actions, mutations, getters)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,17 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    cities: null,
+    error: null,
   },
   getters: {
+    citiesList(state) {
+      return state.cities;
+    },
+    error(state) {
+      return state.error
+    }
   },
   mutations: {
+    setCities(state, payload) {
+      state.cities = payload.cities;
+    },
+    setError(state, payload) {
+      state.error = payload.error;
+    },
   },
   actions: {
+    async getCities(context, payload) {
+      try {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/capital/${payload.city}`
+        );
+        const data = await response.json();
+        if (data.status || data.message) {
+          context.commit('setCities', { cities: null });
+          context.commit('setError', { error: 'Something was wrong. Try search again.' });
+        } else {
+          context.commit('setError', { error: null });
+          context.commit('setCities', { cities: data });
+        }
+      } catch (err: unknown) {
+        console.log('error', err);
+
+        context.commit('setError', { error: 'Something was wrong. Try search again.' });
+      }
+    },
   },
-  modules: {
-  }
-})
+});
