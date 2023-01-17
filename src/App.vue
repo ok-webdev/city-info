21734728171ee3074e576ac875c55fc665b48074
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-17 19:08:45 +0600

    clean App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,8 @@
   import TheHeader from './components/TheHeader.vue';
   export default {
     name: 'App',
-    components: {TheHeader}
-  }
+    components: { TheHeader },
+  };
 </script>
 
-<style lang="scss">
-  *,
-  *::before,
-  *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
-</style>
+<style lang="scss"></style>
