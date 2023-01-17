2c611462d234ac3f143558387231fe2132ffa6d8
Oleg K <oleg.kokoshinskii@gmail.com>
2023-01-17 19:09:05 +0600

    add TheLoader.vue

---
+++ src/components/TheLoader.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="loader">Loading...</p>
+</template>
+
+<style scoped lang="scss">
+  .loader {
+    font-size: 36px;
+    color: gray;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    animation: blinking 1s infinite linear;
+  }
+
+  @keyframes blinking {
+    0% {
+      opacity: 0;
+    }
+
+    100% {
+      opacity: 1;
+    }
+  }
+</style>
